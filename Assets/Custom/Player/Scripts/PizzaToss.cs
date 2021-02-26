20485abaeeee11f6f421c212c0f7748d49a92672
DESKTOP-JOHN\johne <john.entrieri@gmail.com>
2021-02-25 20:21:22 -0500

    Player Hit Sounds & Blink Animations

--- Assets/Custom/Player/Scripts/PizzaToss.cs
+++ Assets/Custom/Player/Scripts/PizzaToss.cs
@@ -7,17 +7,24 @@ public class PizzaToss : MonoBehaviour
 {
     [SerializeField] GameObject pizzaPrefab;
     [SerializeField] int playerHealth = 10;
+    [SerializeField] AudioClip hurtAudioClip;
 
     private MinifigController minifigController;
     private Animator minifigAnimator;
+    private Renderer[] playerRenderers;
+    private AudioSource audioSource;
     private float minifigForwardSpeed;
     private int pizzaTokens = 0;
-
+    [SerializeField] float blinkTime = 0.5f;
+    [SerializeField]  float blinkPeriod = 0.1f;
+    private bool isBlinking = false;
 
     void Start()
     {
         minifigController = GetComponent<MinifigController>();
         minifigAnimator = GetComponent<Animator>();
+        playerRenderers = GetComponentsInChildren<Renderer>();  
+        audioSource = GetComponent<AudioSource>();      
         minifigForwardSpeed = minifigController.maxForwardSpeed;
     }
 
@@ -30,11 +37,26 @@ public class PizzaToss : MonoBehaviour
         }
     }
     public void InflictDamage(int dmg) {
+        if (isBlinking) { return; }
+        audioSource.PlayOneShot(hurtAudioClip);
         playerHealth -= dmg;
+        StartCoroutine( Blink() );        
+
         if (playerHealth <= 0) { 
             playerHealth = 0;
             ProcessDeath(); 
         }
+    }
+
+    private IEnumerator Blink() {
+        isBlinking = true;
+        float blinkTimer = blinkTime;
+        while ( blinkTimer > 0) {
+            foreach(Renderer r in playerRenderers) { r.enabled = !r.enabled; }
+            blinkTimer -= blinkPeriod;
+            yield return new WaitForSeconds(blinkPeriod);
+        }
+        isBlinking = false;
     }
 
     private void ProcessDeath() {
@@ -42,6 +64,7 @@ public class PizzaToss : MonoBehaviour
     }
     
     private void TossComplete() {
+        // Triggered on Animation Event (Toss)
         minifigController.maxForwardSpeed = minifigForwardSpeed;
     }
 
