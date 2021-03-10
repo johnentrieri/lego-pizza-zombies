13bb1d4d9846c101ba279f7ba3ea6372322719f1
DESKTOP-JOHN\johne <john.entrieri@gmail.com>
2021-03-10 11:23:34 -0500

    Added Item Descriptions to the Shop Closes #34

---
+++ Assets/Custom/UI/Scripts/ItemDescription.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemDescription : MonoBehaviour, ISelectHandler
+{
+    [SerializeField] string shortItemDescription;
+    [SerializeField] string longItemDescription;
+    [SerializeField] Text shortTextObject;
+    [SerializeField] Text longTextObject;
+
+    public void OnSelect(BaseEventData eventData) {
+        if(eventData.selectedObject == gameObject)
+         {
+            shortTextObject.text = shortItemDescription;
+            longTextObject.text = longItemDescription;
+         }
+    }
+}
